4db348dd6614c2d626f54740862d49976f00b8a9
Viuviu42 <lengyelcsongi@gmail.com>
2024-04-26 11:21:48 +0200

    4. feladat kesz (regebben 3.masnál 2 is kész lett)

--- fuvar.py
+++ fuvar.py
@@ -4,16 +4,25 @@ class Taxi:
         self.start = start
         self.leng = leng
         self.km = km
-        self.dollar = dollar
+        self.dollar = float(dollar)
         self.plus = plus
         self.howbuy = howbuy
 
 
 taxi = []
 file = open("fuvar.csv", "rt", encoding="utf-8")
-file.readline
+count = 0
+money = 0
+file.readline()
 for row in file:
-    row = row.strip().split(";")
+    row = row.strip().replace(",", ".").split(";")
     taxi.append(Taxi(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
 
 print(f"3. feladat: {len(taxi)} fuvar")
+
+for i in taxi:
+    if i.ID == "6185":
+        count += 1
+        money += i.dollar
+
+print(f"4. feladat: {count} fuvar alatt: {money}$")
